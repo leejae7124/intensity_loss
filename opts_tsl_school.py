a1675ccefe0e7dca2c9d0e00e278ad3c0c988b4b
leessu <leejae7124@gmail.com>
2026-04-24 03:09:19 +0900

    update

--- opts_tsl_school.py
+++ opts_tsl_school.py
@@ -68,7 +68,7 @@ def parse_opts():
                  default=12,
                  type=int),
             dict(name='--loss_func',
-                 default='ce',
+                 default='ce_intensity',
                  type=str,
                  help='ce | pcce_ve8'),
             dict(name='--learning_rate',
